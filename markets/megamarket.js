84bced2162db609ecf3d4be706fe7f268e330ec2
Seorgiy <george.sergeev2014@gmail.com>
2024-04-21 15:32:22 +0300

    fix mm

--- markets/megamarket.js
+++ markets/megamarket.js
@@ -6,7 +6,7 @@ const collect = () => {
   try {
     if (document.querySelector('.catalog-listing-controls input')?.value == 'Лучшее совпадение' && location.hash.includes('sort=1')) return
     if (document.querySelector('.catalog-listing-controls span')?.innerText == 'Лучшее совпадение'&& location.hash.includes('sort=1')) return
-  
+
     let nodes = document.querySelectorAll('div[data-product-id]')
 
     for (let i=0;i<nodes.length;i++) {
@@ -14,12 +14,12 @@ const collect = () => {
       if (nodes[i].className.includes('catalog-item_out-of-stock')) continue;
 
       let item = new Item(
-        nodes[i].querySelector('.item-title > a')?.innerText,
-        nodes[i].querySelector('.item-price > span')?.innerText, 
-        nodes[i].querySelector('.item-title > a')?.href,
+        nodes[i].querySelector('a[data-product-id][title]')?.innerText,
+        nodes[i].querySelector('.item-price > span , .catalog-item-regular-desktop__price')?.innerText, 
+        nodes[i].querySelector('a[data-product-id][title]')?.href,
         'Мегамаркет',
         nodes[i].querySelector('.catalog-item-delivery__text')?.innerText,
-        { cashback: parseInt(nodes[i].querySelector('.bonus-amount')?.innerText.replaceAll(new RegExp(/\s/g),'')  ?? 0, 10) }
+        { cashback: parseInt(nodes[i].querySelector('.bonus-amount')?.innerText?.replaceAll(new RegExp(/\s/g),'')  ?? 0, 10) }
       )
 
 
@@ -37,14 +37,13 @@ const megaFinish = (items) => {
 }
 
 const collectWithDelay = () =>{
-  sendItemTimer = setInterval(collect, 1000); 
+  sendItemTimer = setInterval(collect, 500); 
 }
 
 const observe = (mutations) => {
   for(let mutation of mutations) {
     if (mutation?.addedNodes[0]?.tagName == 'IFRAME' && sendItemTimer == undefined){
-      itemsObserver.disconnect()
-      setTimeout(collectWithDelay,1000)
+      setTimeout(collectWithDelay,2000)
     }
   }
 }
